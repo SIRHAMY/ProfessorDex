8e0f90899094c477e339bea50a05093d82441dac
Hamilton Greene <hamilton.greene@gmail.com>
2016-05-21 14:59:18 -0400

    Added connection console logs

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ require('./environment.js');
 var express = require('express');
 var app = express();
 
+var debug = true;
+
 console.log("ProfessorDex starting up.");
 
 app.get('/webhook', function (req, res) {
-  if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
-    res.send(req.query['hub.challenge']);
-  } else {
-    res.send('Error, wrong validation token');    
-  }
+    
+    if(debug) console.log("Received webhook call");
+    
+    if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+        res.send(req.query['hub.challenge']);
+        if(debug) console.log("Caller verified");
+    } else {
+        res.send('Error, wrong validation token');   
+        if(debug) console.log("Caller verification failed"); 
+    }
 });
 
 console.log("ProfessorDex listening...");
